3f86ec9cea4021005626f4d86a8797ac03a1c271
StanislavStaev <stanislavstaev09@gmail.com>
2024-12-16 14:04:56 +0200

    Add files via upload

---
+++ metodi/zad1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodi
+{
+    internal class zad1
+    {
+
+        static void Main(string[] args)
+        {
+            ime();
+        }
+        static void ime()
+        {
+            var QQQ = Console.ReadLine();
+            Console.WriteLine("Hello "+QQQ);
+
+        }
+    }
+}
