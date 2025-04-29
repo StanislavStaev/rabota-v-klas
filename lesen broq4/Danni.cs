6f37297802903457fe5e5101840aa3f7a30160ee
StanislavStaev <stanislavstaev09@gmail.com>
2025-04-29 12:37:18 +0300

    Add files via upload

---
+++ lesen broq4/Danni.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesen_broq4
+{
+    internal class Danni
+    {
+        private string ime;
+        private decimal zaplata;
+        public string Ime
+        {
+            get {return ime;}
+            set {ime = value;}
+        }
+        public decimal Zaplata
+        {
+            get{return zaplata;}
+            set { zaplata = value;}
+        }
+        public void Izhod()
+        {
+            Console.WriteLine($"Slujitel {Ime} zaplata {Zaplata}");
+        }
+    }
+}
---
+++ lesen broq4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesen_broq4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Br");
+            int n= int.Parse(Console.ReadLine());
+            Danni[] sluzitel = new Danni[n];
+            // vuvejdane v Main
+            for (int i = 0; i <n; i++) 
+            {
+                Console.WriteLine($"Slujitel{i+1}");
+                sluzitel[i] = new Danni();
+                Console.WriteLine("Ime");
+                sluzitel[i].Ime=Console.ReadLine();
+                Console.WriteLine("Zaplata");
+                sluzitel[i].Zaplata=decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("--Spisak--");
+            for (int i = 0; i < n; i++) 
+            {
+                sluzitel[i].Izhod();
+            }
+        }
+    }
+}
