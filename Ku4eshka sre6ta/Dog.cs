84ecebb0eeeffa102889f41d63184ff74ba9a9a8
StanislavStaev <stanislavstaev09@gmail.com>
2025-06-03 12:22:43 +0300

    Add files via upload

---
+++ Ku4eshka sre6ta/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ku4eshka_sre6ta
+{
+    internal class Dog
+    {
+        private string name;
+        public Dog() 
+        {
+            this.name = "Balkan";
+        }
+        public Dog(string name) 
+        {
+        this.name= name;
+        }
+        public string Name
+        {
+            get { return name; } 
+            set { name = value; }
+        }
+        public void Bark()
+        {
+            Console.WriteLine("ku4eto kaza bau bau i negovoto ime e {0}",name);
+        }
+    }
+}
---
+++ Ku4eshka sre6ta/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ku4eshka_sre6ta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first=null;
+            Console.WriteLine("Vuvedi ime za purvoto ku4e");
+            first=Console.ReadLine();
+            Dog dog = new Dog(first);     
+            Dog second = new Dog();
+            Console.WriteLine("vtoro ime a ku4e");
+            string seconddog=Console.ReadLine();
+            second.Name = seconddog;
+            Dog dog1 = new Dog();
+            Dog[] dogs = new Dog[] { dog, second, dog1 };
+            foreach (Dog dog2 in dogs) 
+            {
+             dog2.Bark();
+            }
+            
+        }
+    }
+}
