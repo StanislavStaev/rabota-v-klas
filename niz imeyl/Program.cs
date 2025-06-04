e54bed79606e8b85e8f1c81cb9da949a024ff0b5
StanislavStaev <stanislavstaev09@gmail.com>
2025-06-04 13:52:12 +0300

    Add files via upload

---
+++ niz imeyl/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace niz_imeyl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vuvedi email");
+            string email=Console.ReadLine();
+            int firstIndex= email.IndexOf('@');
+            int lastIndex = email.IndexOf("com");
+            if (firstIndex -1 >=lastIndex-firstIndex) 
+            {
+                Console.WriteLine("Call her");
+            }
+            else
+            {
+                Console.WriteLine("She is not the one");
+            }
+        }
+    }
+}
